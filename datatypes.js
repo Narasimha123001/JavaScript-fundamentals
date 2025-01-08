fb2074512f9d037a579fbeb8dff0d8db529b99e3
Narasimha <narasimhaanguluri001@gmail.com>
2025-01-08 23:18:21 +0530

    DataTypes in javaScripts

---
+++ datatypes.js
@@ -0,0 +1,28 @@
+let number  = 42;         //Number
+let text = "Hello";       //String
+let isTrue = true;        //Boolean
+let nothing = null;       //Null
+let notDefined;           //Undefined
+
+
+let person = {            //Object
+    name: "narasimha",
+    age : 21,
+    dept : 'CSE'
+};
+
+let numberArray = [1,2,3];  //Array
+
+
+
+//for display
+
+console.log(`Number: ${number}`);
+console.log(`Text: ${text}`);
+console.log(`isTrue ${isTrue}`);
+console.log(`nothingVale ${nothing}`);
+console.log(`Not Define: ${notDefined}`);
+
+console.log('Person Details' ,person);
+
+
